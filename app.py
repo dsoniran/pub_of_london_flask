bfbe37d9f23dc4847fab656912476d1cfce55add
dareshesaidit <78746521+dareshesaidit@users.noreply.github.com>
2021-04-19 22:40:14 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+app = Flask('pub_flask') #can be renamed once uploaded online
+
+#---Dashboard
+@app.route('/')
+def dashboard():
+    return render_template("dashboard.html")
+
+#---Map Page
+@app.route('/map')
+def map_of_london_pubs():
+    return render_template("map.html")
+#---Bar Chart Page
+
+@app.route('/pub-chart')
+def pubchart():
+    return render_template("bar_chart.html")
+
+#--- Pie Chart Page
+@app.route('/pie-chart') #-----Could put the barchart on this page
+def pie_chart():
+   return render_template("pie_chart.html")
+
+#Pie Chart Page
+
+app.run(debug=True)
