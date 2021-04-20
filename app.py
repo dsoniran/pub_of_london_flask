c0b8585e1b46266896d69f72cb8a8f9d00405766
dareshesaidit <78746521+dareshesaidit@users.noreply.github.com>
2021-04-20 19:00:27 +0100

    update of css to centralise images (successful on all except dashboard.html)

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from flask import Flask, render_template
 app = Flask('pub_flask') #can be renamed once uploaded online
 
 #---Dashboard
+
 @app.route('/')
 def dashboard():
     return render_template("dashboard.html")
 
 #---Map Page
+
 @app.route('/map')
 def map_of_london_pubs():
     return render_template("map.html")
+
 #---Bar Chart Page
 
 @app.route('/pub-chart')
@@ -19,7 +22,8 @@ def pubchart():
     return render_template("bar_chart.html")
 
 #--- Pie Chart Page
-@app.route('/pie-chart') #-----Could put the barchart on this page
+
+@app.route('/pie-chart')
 def pie_chart():
    return render_template("pie_chart.html")
 
